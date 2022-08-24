b0c3bb8cdd8ba7284ef0399799b3c4fef7d69703
Brandon Tabaska <btabaska@gmail.com>
2022-08-24 10:48:59 -0700

    fixed netlify state bug

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,11 @@
 import { Box, LinkBox, LinkOverlay, Flex, Text } from "@chakra-ui/layout";
 import { Image, Button } from "@chakra-ui/react";
-import { useStoreActions, useStoreState } from "easy-peasy";
+import { useStoreActions } from "easy-peasy";
 import Link from "next/link";
 
 const Header = () => {
   //  Import state and actions from easy peasy for use within clear button
   const changeCards = useStoreActions((store: any) => store.changeActiveCards);
-  const cards = useStoreState((state: any) => state.activeCards);
   // set up click handler for Clear button
   const handleClick = () => {
     // set changeCards to empty array to clear state of colored cards
